a59e7a66e45cbb8dd2fe80b092b121f54087c5c5
sandhya425 <rajputsandhya425@gmail.com>
2025-04-16 12:42:20 +0530

    delete data

--- post.js
+++ post.js
@@ -16,4 +16,26 @@ async function df() {
         // let b=await a.json()//read
         console.log(b)
     }
+
+
+    //delete
+async function del(arg){
+    await fetch(`http://localhost:4000/product/${arg}`,{
+      method:"DELETE"
+    })
+}
+
+async function f() {
+    let data=await fetch('http://localhost:4000/product');
+    let res=await data.json();
+    
+
+    let o=document.querySelector('#output')
+    o.innerHTML=res.map(e=>`<div>${e.name}</div>
+<div>${e.email}</div>
+<div>${e.number}</div>
+<div>${e.address}</div>
+<div>${e.password}</div>
+<button onclick="del('${e.id}')"> remove</button>`).join(" ")
+}
     
--- ragister.js
+++ ragister.js
@@ -21,3 +21,26 @@ async function rl() {
     })
     console.log(k)
 }
+
+
+//delete
+async function del(arg){
+    await fetch(`http://localhost:4000/product/${arg}`,{
+      method:"DELETE"
+    })
+}
+
+
+async function f() {
+    let data=await fetch('http://localhost:4000/product');
+    let res=await data.json();
+    
+
+    let o=document.querySelector('#output')
+    o.innerHTML=res.map(e=>`<div>${e.name}</div>
+<div>${e.email}</div>
+<div>${e.number}</div>
+<div>${e.address}</div>
+<div>${e.password}</div>
+<button onclick="del('${e.id}')"> remove</button>`).join(" ")
+}
