70d9e261dcb65b9fb1adc560a93d1251beb5372b
sandhya425 <rajputsandhya425@gmail.com>
2025-04-19 16:08:08 +0530

    dash

--- practice.js
+++ practice.js
@@ -7,30 +7,78 @@
 
 
 //post
-async function rj() {
-    let name=document.querySelector("#name").value
-    let email=document.querySelector("#email").value
-   let ob ={
+// async function rj() {
+//     let name=document.querySelector("#name").value
+//     let email=document.querySelector("#email").value
+//    let ob ={
+//     "name":name,
+//     "email":email
+//    }
+//    let a=await fetch ('http://localhost:4000/register',{
+//     method:"POST",
+//     headers:{
+//         "content-type":"application/json",
+//     },
+//     body:JSON.stringify(ob)
+//    })
+//   console.log(a)
+// }
+
+
+
+//delete
+// async function del(arg){
+//     await fetch (`http://localhost:4000/register/${arg}`,{
+//         method:"DELETE"
+//     })
+// }
+// async function kl(){
+//     let a=await fetch('http://localhost:4000/register')
+//     let y=await a.json()
+
+//     let c=document.querySelector("#output")
+//     c.innerHTML=y.map(e=>`
+//        <div>${e.name}</div>
+//        <div>${e.email}</div>
+//        <button onclick="del('${e.id}')">remove</button> `)
+// }
+
+
+//update
+
+async function final(id) {
+   let name=document.querySelector("#name").value 
+   let email=document.querySelector("#email").value
+   let obj={
     "name":name,
     "email":email
    }
-   let a=await fetch ('http://localhost:4000/register',{
-    method:"POST",
+fetch (`http://localhost:4000/register/${id}`,{
+    method:"PUT",
     headers:{
-        "content-type":"application/json",
+       "content-type":"application/json"
     },
-    body:JSON.stringify(ob)
-   })
-  console.log(a)
-}
-//delete
-async function kl() {
-    let data=await fetch('http://localhost:4000/register')
-    let re=await data.json();
+    body:JSON.stringify(obj)
+})
 
-    let h=document.querySelector("#output")
-    h.innerHTML=re.map(e=>`<div>${e.name}</div
-     <div>${e.email}</div 
-     <button onclock="  `)
 }
->
+async function up(id){
+let x=await fetch (`http://localhost:4000/register/${id}`)
+    let y=await x.json()
+    let f=`<form><input type="text" placeholder="enter"id="name"value="${y.name}"
+    <input type="text" placeholder="enter"id="email"value="${y.email}"
+    <button onclick="final('${id}')">update</button>
+    </form>`
+    let h=document.querySelector("#form")
+    h.innerHTML=f
+}
+async function kl(){
+    let x=await fetch('http://localhost:4000/register')
+    let y=await x.json()
+
+    let a=document.querySelector("#output")
+    a.innerHTML=y.map(a=>`
+       <div>${a.name}</div>
+       <div>${a.email}</div>
+<button onclick="up('${a.id}')">update</button> `)
+}
